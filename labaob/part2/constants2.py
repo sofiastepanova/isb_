f17d43b41c881b2b3cd293bb99751dfde00ae52b
sofiastepanova <159649742+sofiastepanova@users.noreply.github.com>
2025-04-18 01:10:01 +0400

    Update constants2.py

--- labaob/part2/constants2.py
+++ labaob/part2/constants2.py
@@ -1,5 +1,5 @@
-INPUT_ENCODED_FILE = 'cod17.txt'
 FREQUENCIES_FILE = 'russian_letter_frequency.json'
+INPUT_ENCODED_FILE = 'cod17.txt'
+KEY_FILE = "key.json"
 OUTPUT_FREQUENCIES_FILE = 'encoded_frequencies.json'
-OUTPUT_MAPPING_FILE = 'mapping.json'
 OUTPUT_DECRYPTED_FILE = 'decrypted.txt'
