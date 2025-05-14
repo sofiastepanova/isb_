f6713e38175633d1072a804781b267de42acda60
sofiastepanova <159649742+sofiastepanova@users.noreply.github.com>
2025-05-14 19:49:20 +0400

    Update test.py

--- test.py
+++ test.py
@@ -38,24 +38,6 @@ def same_bits_test(bits):
 
 
 
-def consecutive_runs_test(bits: str) -> float:
-    """
-    Тест на одинаково подряд идущие биты.
-    :param sequence: Бинарная строка.
-    :return: p-значение.
-    """
-    n = len(bits)
-    p = bits.count('1') / n
-
-    if abs(p - 0.5) >= 2 / math.sqrt(n):
-        return 0.0
-
-    rr = sum(1 for i in range(1, n) if bits[i] != bits[i - 1])
-
-    numerator = abs(rr - 2 * n * p * (1 - p))
-    denominator = 2 * math.sqrt(2 * n) * p * (1 - p)
-    return math.erfc(numerator / denominator)
-
 
 def longest_sequence_test(data):
     """
