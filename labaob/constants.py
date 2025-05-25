3752352ddffa58ad53092a656daa7150a199a92b
sofiastepanova <159649742+sofiastepanova@users.noreply.github.com>
2025-05-25 14:17:26 +0400

    Delete labaob directory

--- labaob/constants.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-INPUT_FILE_PATH = os.path.join('part1', 'original_text.txt')
-OUTPUT_ENCRYPTED_FILE_PATH = os.path.join('part1', 'encrypted_text.txt')
-OUTPUT_KEY_FILE_PATH = os.path.join('part1', 'cipher_key.json')
-ALPHABET = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ '
